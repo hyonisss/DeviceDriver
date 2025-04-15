5b153cec1c6abf164e21101072bda7cf6c6ea5ce
wbin-lee <gyeun1212@gmail.com>
2025-04-15 10:06:53 +0900

    add read fuction

--- device_driver.cpp
+++ device_driver.cpp
@@ -1,4 +1,5 @@
 #include "device_driver.h"
+#include <stdexcept> // 예외 사용 시 필요
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice *hardware) : m_hardware(hardware)
 {
@@ -7,7 +8,23 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice *hardware) : m_hardware(hardware)
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    const int readCount = 5;
+    int values[readCount];
+
+    for (int i = 0; i < readCount; ++i)
+    {
+        values[i] = static_cast<int>(m_hardware->read(address));
+    }
+
+    for (int i = 1; i < readCount; ++i)
+    {
+        if (values[i] != values[0])
+        {
+            throw std::runtime_error("Read failed: values are not consistent.");
+        }
+    }
+
+    return values[0]; // 모든 값이 같을 때만 반환
 }
 
 void DeviceDriver::write(long address, int data)
--- main.cpp
+++ main.cpp
@@ -1,13 +1,42 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
 
-TEST(DeviceDriver, ReadFromHW)
+using namespace testing;
+
+class FlashMock : public FlashMemoryDevice
+{
+public:
+    MOCK_METHOD(unsigned char, read, (long address), (override));
+    MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+TEST(DeviceDriver, ReadSucceedsWhenAllValuesAreSame)
 {
     // TODO : replace hardware with a Test Double
-    FlashMemoryDevice *hardware = nullptr;
-    DeviceDriver driver{hardware};
-    int data = driver.read(0xFF);
-    EXPECT_EQ(0, data);
+    FlashMock mock;
+    EXPECT_CALL(mock, read(0xBB))
+        .Times(5)
+        .WillRepeatedly(Return(0xAB));
+
+    DeviceDriver driver{&mock};
+    int result = driver.read(0xBB);
+    EXPECT_EQ(result, 0xAB);
+}
+
+// 값이 달라서 예외 발생하는 경우
+TEST(DeviceDriver, ReadFailsWhenValuesDiffer)
+{
+    // TODO : replace hardware with a Test Double
+    FlashMock mock;
+    EXPECT_CALL(mock, read(0xBB))
+        .WillOnce(Return(0xAA))
+        .WillOnce(Return(0xAA))
+        .WillOnce(Return(0xAB)) // 중간에 다름!
+        .WillOnce(Return(0xAA))
+        .WillOnce(Return(0xAA));
+
+    DeviceDriver driver{&mock};
+    EXPECT_THROW({ driver.read(0xBB); }, std::runtime_error); // 또는 ReadFailException (정의한 경우)
 }
 
 int main()
